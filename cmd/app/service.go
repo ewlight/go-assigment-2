02d03a433b0f2eee4823275b53d5a5a6c847af30
Eka Widiantara <eka@motionpay.id>
2022-10-07 09:53:16 +0700

    Create Service Module

--- cmd/app/service.go
+++ cmd/app/service.go
@@ -34,15 +34,59 @@ func (c ConnectionDB) getOrderList() ([]model.Order, error, int64) {
 }
 
 func (c ConnectionDB) addOrder(Order *model.Order, newOrder resources.InputOrder) error {
-	panic("implement me")
+	if Order.Id != 0 {
+		var existingItem []model.Item
+		c.connection.Where("order_id = ?", Order.Id).Find(&existingItem)
+		var existingItemCount int = len(existingItem)
+		var newItemCount int = len(newOrder.Items)
+		if newItemCount < existingItemCount {
+			c.connection.Debug().Unscoped().Model(model.Item{}).Where("order_id = ?", Order.Id).
+			Order("id asc").Limit(existingItemCount - newItemCount).Offset(newItemCount).Delete(&model.Item{})
+		}
+	}
+	Order.CustomerName = newOrder.CustomerName
+	err := c.connection.Save(Order).Error
+	if err != nil {
+		return err
+	}
+
+	for eachIndex, eachItem := range newOrder.Items {
+		var item model.Item
+		c.connection.Where("order_id = ?", Order.Id).Order("id asc").Limit(1).Offset(eachIndex).Find(&item)
+		item.OrderID = Order.Id
+		item.ItemCode = eachItem.ItemCode
+		item.Description = eachItem.Description
+		item.Quantity = eachItem.Quantity
+		err := c.connection.Save(&item).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (c ConnectionDB) getOrderDetailById(id uint, preload bool) (model.Order, error) {
-	panic("implement me")
+	var order model.Order
+	conn := c.connection.Where("id = ?", id)
+	if preload {
+		conn = conn.Preload("Items")
+	}
+	conn = conn.First(&order)
+	err := conn.Error
+	if err != nil {
+		return order, err
+	}
+	return order, err
+
 }
 
 func (c ConnectionDB) deleteOrder(id int) error {
-	panic("implement me")
+	var order model.Order
+	err := c.connection.Delete(&order, id).Error
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 
